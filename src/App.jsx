ecf8c8be90d4b5773da25611b09941c3be04811f
Josilandio <landdio@tuta.io>
2024-06-25 14:36:22 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h3><samp>HELLO, MY NAME IS <span class="bg-name">JOSILANDIO</span>!</samp></h3>
+      <div className="card">
+      <p className="read">
+        I'm a student at the Federal University of Piauí, working as a software engineer (<a href="https://github.com/josilandio" target='_blank'>github</a>) and designer (<a href="https://behance.com/josilandio" target='_blank'>behance</a>). You can find more about me on <a href="https://linkedin.com/in/josilandio" target='_blank'>linkedin</a>.
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
